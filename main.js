f3a6ac2ce488be298ac04dfa574e232e3c6b4be2
R <rianipad2@gmail.com>
2024-05-16 20:33:48 +0700

    fps camera
    cam

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as THREE from "three"
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,19 +10,144 @@ renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(-5, 0, 0);
-camera.lookAt(0, 0, 0);
+scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0,0,0);
-controls.update();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(-5, 1, 0);
+camera.lookAt(0, 0, 0);
+const controls = new PointerLockControls(camera, renderer.domElement);
+
+
+const objects = [];
+let raycaster;
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
+const blocker = document.getElementById( 'blocker' );
+const instructions = document.getElementById( 'instructions' );
+
+
+instructions.addEventListener( 'click', function () {
+
+    controls.lock();
+
+} );
+
+controls.addEventListener( 'lock', function () {
+
+    instructions.style.display = 'none';
+    blocker.style.display = 'none';
+
+} );
+
+controls.addEventListener( 'unlock', function () {
+
+    blocker.style.display = 'block';
+    instructions.style.display = '';
+
+} );
+
+scene.add( controls.getObject() );
+
+const onKeyDown = function ( event ) {
+
+    switch ( event.code ) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+
+        case 'Space':
+            if ( canJump === true ) velocity.y += 30;
+            canJump = false;
+            break;
+
+    }
+
+};
+
+const onKeyUp = function ( event ) {
+
+    switch ( event.code ) {
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+
+    }
+
+};
+
+const onWindowResize = function() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+document.addEventListener( 'keydown', onKeyDown );
+document.addEventListener( 'keyup', onKeyUp );
+window.addEventListener( 'resize', onWindowResize );
+
+raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+
+var planeGeo = new THREE.PlaneGeometry(400, 400);
+var planeMat = new THREE.MeshPhongMaterial({color: 0x777777, side:THREE.DoubleSide});
+var plane = new THREE.Mesh(planeGeo, planeMat);
+scene.add(plane);
+plane.rotation.set(Math.PI/2, 0, 0);
+plane.position.set(0, 0, 0);
+
+plane.castShadow = true;
+plane.receiveShadow = true;
 
 var sunGeo = new THREE.SphereGeometry(1, 10, 10);
 var sunMaterial = new THREE.MeshPhongMaterial({color: 0xffff33});
 var sun = new THREE.Mesh(sunGeo, sunMaterial);
 scene.add(sun);
-sun.position.set(5, 0, 0)
+sun.position.set(5, 5, 0)
 sun.scale.set(0.5, 0.5,0.5);
 sun.castShadow = true;
 sun.receiveShadow = true;
@@ -46,16 +171,6 @@ moon.scale.set(0.6, 0.6, 0.6);
 
 moon.castShadow = true;
 moon.receiveShadow = true;
-
-var planeGeo = new THREE.PlaneGeometry(40, 40);
-var planeMat = new THREE.MeshPhongMaterial({coolor: 0x777777, side:THREE.DoubleSide});
-var plane = new THREE.Mesh(planeGeo, planeMat);
-scene.add(plane);
-plane.rotation.set(Math.PI/2, 0, 0);
-plane.position.set(0, -3, 0);
-
-plane.castShadow = true;
-plane.receiveShadow = true;
 
 // var ambientLight = new THREE.AmbientLight(0xFF6666);
 // scene.add(ambientLight);
@@ -133,7 +248,59 @@ function animate() {
     earth.rotation.y += 0.05;
 
 	// const delta = clock.getDelta();
+
 	// if ( mixer ) mixer.update( delta );
+
+    const time = performance.now();
+
+    //movement
+    if ( controls.isLocked === true ) {
+
+        raycaster.ray.origin.copy( controls.getObject().position );
+        raycaster.ray.origin.y -= 10;
+
+        const intersections = raycaster.intersectObjects( objects, false );
+
+        const onObject = intersections.length > 0;
+
+        const delta = ( time - prevTime ) / 1000;
+
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        velocity.y -= 9.8 * 10.0 * delta; // 100.0 = mass
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+        if ( onObject === true ) {
+
+            velocity.y = Math.max( 0, velocity.y );
+            canJump = true;
+
+        }
+
+        controls.moveRight( - velocity.x * delta / 2);
+        controls.moveForward( - velocity.z * delta / 2);
+
+        controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+        if ( controls.getObject().position.y < 1.7 ) {
+
+            velocity.y = 0;
+            controls.getObject().position.y = 1.5;
+
+            canJump = true;
+
+        }
+
+    }
+
+    prevTime = time;
 
     requestAnimationFrame(animate);
 }
